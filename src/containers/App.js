7b03a9c45506084d37947b8c065aa1d32d683df5
Isaac Elmore <isaac.elmore@gmail.com>
2022-04-06 22:46:55 +0100

    Basic hooks working.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from "../components/CardList";
 // import { cats } from "./cats";
 import SearchBox from "../components/SearchBox";
@@ -6,48 +6,37 @@ import Scroll from "../components/Scroll";
 import './App.css';
 import ErrorBoundary from "../components/ErrorBoundary";
 
-class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            cats: [],
-            searchField: ''
-        }
-    }
+function App() {
+    const [cats, setCats] = useState([]);
+    const [searchField, setSearchField] = useState("")
 
-    onSearchChange = (event) => {
-        this.setState({ searchField: event.target.value })
-    }
-
-    render() {
-        const { cats, searchField } = this.state;
-
-        const filteredCats = cats.filter(cat => {
-            return cat.name.toLowerCase().includes(searchField.toLowerCase())
-        })
-
-        if (cats.length === 0) {
-            return <h1>Loading...</h1>
-        }
-
-        return (
-            <div className={'tc'}>
-                <h1 className={'f2'}>Cat Friends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <Scroll>
-                    <ErrorBoundary>
-                        <CardList cats={filteredCats}/>
-                    </ErrorBoundary>
-                </Scroll>
-            </div>
-        )
-    }
-
-    componentDidMount() {
+    useEffect(()=> {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState({ cats: users }))
+            .then(users => setCats(users));
+    }, []);
+
+    const onSearchChange = (event) => setSearchField(event.target.value);
+
+    const filteredCats = cats.filter(cat => {
+        return cat.name.toLowerCase().includes(searchField.toLowerCase())
+    });
+
+    if (cats.length === 0) {
+        return <h1>Loading...</h1>
     }
+
+    return (
+        <div className={'tc'}>
+            <h1 className={'f2'}>Cat Friends</h1>
+            <SearchBox searchChange={onSearchChange}/>
+            <Scroll>
+                <ErrorBoundary>
+                    <CardList cats={filteredCats}/>
+                </ErrorBoundary>
+            </Scroll>
+        </div>
+    );
 }
 
 export default App;
